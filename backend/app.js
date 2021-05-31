3bce85123254d7ab8bd2a855bb29dbeee5f66f06
Le Bosser <jeremy.lebosser@gmail.com>
2021-05-31 16:40:04 +0200

    build [backend]: installation mongoose et connection mongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://Galad:j3r3myl3arn@cluster0.sf1z2.mongodb.net/test?retryWrites=true&w=majority',
+    {   useNewUrlParser: true,
+        useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+//middleware qui permet d'accéder à notre API peut importe l'origine
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
 
 app.use((req, res) => {
     res.json({ message: 'Votre requete a bien été reçue !' });
