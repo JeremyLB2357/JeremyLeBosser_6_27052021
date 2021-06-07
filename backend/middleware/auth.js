73d9787e31c08ee7b54c41acab7e2d97092c13d4
Le Bosser <jeremy.lebosser@gmail.com>
2021-06-07 19:27:45 +0200

    feat [backend]: ajout authentification sur toutes les routes

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,7 +1,34 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
 module.exports = (req, res, next) => {
     try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        const userId = decodedToken.userId;
+        User.findOne({ _id: userId })
+            .then(() => {
+                console.log('je passe à la suite');
+                next();
+            })
+            .catch(() => {
+                throw 'Invalid user ID';
+            });
+    } catch {
+        res.status(401).json({
+            error: new Error('Invalid request!')
+        });
+    }
+
+    /*if (req.body.userId !== userId) {
+        console.log('je ne te connais pas')
+    } else {
+        console.log('ça marche');
+    }
+    res.status(201).json({
+        message: 'je teste'
+    });*/
+    /*try {
         const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
         const userId = decodedToken.userId;
@@ -14,5 +41,5 @@ module.exports = (req, res, next) => {
         res.status(401).json({
             error: new Error('Invalid request!')
         });
-    }
+    }*/
 };
